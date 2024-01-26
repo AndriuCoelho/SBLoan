0616f6799441985dc879b4e556ec42de24fa7a36
ikyrie <mah.c5r@gmail.com>
2024-01-26 14:59:22 -0300

    first commit

---
+++ SBLoanView.swift
@@ -0,0 +1,18 @@
+//
+//  SBLoanView.swift
+//  Pods
+//
+//  Created by ALURA on 26/01/24.
+//
+
+import SwiftUI
+
+struct SBLoanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SBLoanView()
+}
